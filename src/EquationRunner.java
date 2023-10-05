f46f6ecb0f565ad2daada7960a147ecec630e303
w <w@>
2023-10-04 21:52:47 -0400

    Fixed the method slope, added comments to the EquationRunner.

--- src/EquationRunner.java
+++ src/EquationRunner.java
@@ -3,11 +3,15 @@ import java.util.Scanner;
 
 public class EquationRunner {
     public static void main(String[] args) {
+        // Print statements and user input
+
+
         Scanner s = new Scanner(System.in);
         System.out.print("Enter your first Coordinate Point: ");
         String first = s.nextLine();
         System.out.print("Enter your second Coordinate Point: ");
         String second = s.nextLine();
+
 
         // Separate x's and y's from the coordinate point form
         String updatedFirst = first.substring(1, first.length() - 1);
@@ -16,10 +20,13 @@ public class EquationRunner {
         String tempy1 = updatedFirst.substring(updatedFirst.indexOf(",") + 1, updatedFirst.length());
         String tempx2 = updatedSecond.substring(0, updatedSecond.indexOf(","));
         String tempy2 = updatedSecond.substring(updatedSecond.indexOf(",") + 1, updatedSecond.length());
+        // Converting from string to integer
         int x1 = Integer.parseInt(tempx1);
         int y1 = Integer.parseInt(tempy1);
         int x2 = Integer.parseInt(tempx2);
         int y2 = Integer.parseInt(tempy2);
+
+        // Call the LinearEquation class constructor
         LinearEquation done = new LinearEquation(x1, y1, x2, y2);
         System.out.print(done.toString());
         System.out.print("\nEnter a third x-value: ");
--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,10 +1,13 @@
 import java.lang.Math;
 public class LinearEquation {
+
+
     // instance variables
     private int x1;
     private int x2;
     private int y1;
     private int y2;
+
 
     // constructor
    public LinearEquation(int x1, int y1, int x2, int y2){
@@ -14,6 +17,7 @@ public class LinearEquation {
        this.y2 = y2;
     }
 
+
     // methods
     public String firstPair(){
        return "(" + x1 + ", " + y1 + ")";
@@ -21,31 +25,34 @@ public class LinearEquation {
 
 
     public String secondPair(){
-        return "(" + x2 + ", " + y2 + ")";
+       return "(" + x2 + ", " + y2 + ")";
     }
 
 
     public double slope(){
-        return (y2 - y1) / (x2 - x1);
+       return ((double)y2 - (double)y1) / ((double)x2 - (double)x1);
     }
 
 
     public double distanceBetweenPoints(){
-        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+       return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
     }
 
 
     public double yIntercept(){
-        return y1 - slope() * x1;
+       return y1 - slope() * x1;
     }
 
 
     public String slopeInterceptForm(){
-        return "y = " + (y2 - y1) + "/" + (x2 - x1) + "x + " + yIntercept();
+       return "y = " + (y2 - y1) + "/" + (x2 - x1) + "x + " + yIntercept();
     }
+
+
     public double solve(double x){
-        return (slope() * x + yIntercept());
+       return (slope() * x + yIntercept());
     }
+
 
     public String toString(){
         return "First pair: " + firstPair() + "\nSecond pair: " + secondPair() + "\nSlope of line: " + slope() + "\nY-intercept: " + yIntercept() + "\nSlope Intercept form: " + slopeInterceptForm() + "\nDistance between points: " + String.format("%.2f", distanceBetweenPoints());
